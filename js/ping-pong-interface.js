eb4d5443c7d72a8a54e74d57a3c67121f50bbccc
sarah leahy <sarah leahy>
2017-03-10 16:52:01 -0700

    create gitignore file, add browserify and gulp

--- js/ping-pong-interface.js
+++ js/ping-pong-interface.js
@@ -1,7 +1,9 @@
+var Calculator = require('./../js/scripts.js').calculatorModule;
+
 $(document).ready(function() {
     $('.ping-pong').submit(function(event) {
         event.preventDefault();
-        var newCalculator = new Calaculator("hot pink");
+        var newCalculator = new Caluclator("hot pink");
         var number = $("#number").val();
         var output = newCalculator.pingPong(number);
         output.forEach(function(element) {
